a4465783f8e96c36743c2541bab6f875b8aaefa3
lucadisanto7 <lucadisanto47@gmail.com>
2024-06-25 14:46:05 +0200

    [UPD] Declared other 2 variables to javascript.js

--- script/javascript.js
+++ script/javascript.js
@@ -3,12 +3,12 @@ let chilometri = prompt('inserisci i chilometri da percorrere');
 
 let prezzo_biglietto = 0.21 * chilometri;
 let sconto = 0
+let prezzo_scontato = 0
+let prezzo_finale = 0
 
 if (age < 18){
-    sconto = 20
+    sconto = 20;
 }
 else if (age > 65){
-    sconto = 40
+    sconto = 40;
 }
-
-
